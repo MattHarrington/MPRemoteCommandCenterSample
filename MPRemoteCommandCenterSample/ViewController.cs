58d4494612baf3a841b04c366560203bb92a104d
Matt Harrington <mattharrington@users.noreply.github.com>
2015-07-30 09:18:31 -0700

    Play stream in background, show NowPlaying

--- MPRemoteCommandCenterSample/ViewController.cs
+++ MPRemoteCommandCenterSample/ViewController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 
 using UIKit;
+using AVFoundation;
+using Foundation;
+using MediaPlayer;
 
 namespace MPRemoteCommandCenterSample
 {
     public partial class ViewController : UIViewController
     {
+        readonly NSUrl audioUrl = NSUrl.FromString("http://vprclassical.streamguys.net/vprclassical128.mp3");
+
         public ViewController(IntPtr handle)
             : base(handle)
         {
@@ -14,14 +19,45 @@ namespace MPRemoteCommandCenterSample
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            var audioSession = AVAudioSession.SharedInstance();
+            audioSession.SetCategory(AVAudioSessionCategory.Playback);
+            audioSession.SetActive(true);
+
+            // This is not working...
+            MPRemoteCommandCenter rcc = MPRemoteCommandCenter.Shared;
+            rcc.SeekBackwardCommand.Enabled = false;
+            rcc.SeekForwardCommand.Enabled = false;
+            rcc.NextTrackCommand.Enabled = false;
+            rcc.PreviousTrackCommand.Enabled = false;
+            rcc.SkipBackwardCommand.Enabled = false;
+            rcc.SkipForwardCommand.Enabled = false;
+            rcc.PlayCommand.Enabled = true;  // http://stackoverflow.com/a/28925369.  You must enable a command so that others can be disabled?
+
+            MPNowPlayingInfo nowPlayingInfo = new MPNowPlayingInfo();
+            nowPlayingInfo.AlbumTitle = "Vermont";
+            nowPlayingInfo.Artist = "Colchester";
+            nowPlayingInfo.Title = "VPR";
+            MPNowPlayingInfoCenter.DefaultCenter.NowPlaying = nowPlayingInfo;
+
+            AVPlayer player = new AVPlayer(audioUrl);
+            player.Play();
         }
 
-        public override void DidReceiveMemoryWarning()
+        public override void ViewDidAppear(bool animated)
         {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            base.ViewDidAppear(animated);
+            UIApplication.SharedApplication.BeginReceivingRemoteControlEvents();
+            this.BecomeFirstResponder();
         }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+            UIApplication.SharedApplication.EndReceivingRemoteControlEvents();
+            this.ResignFirstResponder();
+        }
+            
     }
 }
 
